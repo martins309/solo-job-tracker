83aabfb57824e013c515f7982b5469f3662f42a0
Andrew Martins <andrew_omartins@yahoo.com>
2021-03-17 15:39:20 -0400

    changed from class to functional and api data is now displaying in the browser

--- src/components/JobSearchForm.jsx
+++ src/components/JobSearchForm.jsx
@@ -1,57 +1,60 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import SearchCardList from './SearchCardList';
 
 
-class JobSearchForm extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            searchQuery: "",
-            languageArray: []
-        }
-    }
 
-
-    _onChange = (field, value) => {
-        this.setState({
-            [field]: value
-        })
-    }
-
-
-    _handleSubmit = async (e) => {
+const JobSearchForm = () => {
+   const [search, setSearch] = useState('');
+   const [jobList, setjobList]= useState([]);
+   
+    const _handleSubmit = async (e) => {
         e.preventDefault();
-        const url = `https://jobs.github.com/positions.json?description/${this.state.searchQuery}`;
-        const newSearchData = await fetch(url)
-            .then(response => response.json());
-        this.setState({
-            languageArray: [...this.state.languageArray, newSearchData],
-        });
 
+        const searchData = await fetch(`http://127.0.0.1:3001/search/?url=https://jobs.github.com/positions.json?description=${search}`, {
+            method: 'Get',
+            headers: { 'Content-Type': 'application/json'}
+        }).then(
+            (response) => (response.json())
+        );
+        console.log("Here are the search results: ", searchData);
+        setjobList(searchData);
     }
 
-    render() {
-        const { languageArray } = this.state;
+
+    const _onChange = (e) => {
+        setSearch(e.target.value)
+    }
+
+   
         return (
-            <div>
-                <form onSubmit={this._handleSubmit}>
-                    <label>
-                        <input 
-                        name="searchQuery"
-                        placeholder="Enter a language"
-                        value={this.state.searchQuery}
-                        type="text"
-                        onChange={(e) => {
-                            this._onChange(e.target.name, e.target.value)
-                        }}
-                        />
-                    </label>
-                        <button type="submit">Search</button>
-                </form>
-                {languageArray.length ? <SearchCardList languageArray={languageArray} /> : " "}
-            </div>
+            <>
+                    <div>
+                        <form onSubmit={_handleSubmit}>
+                            <label>
+                                <input 
+                                name="searchQuery"
+                                placeholder="Enter a language"
+                                value={search}
+                                type="text"
+                                onChange={_onChange}
+                                />
+                            </label>
+                                <button type="submit">Search</button>
+                        </form>
+                    </div>
+                        <ul>
+                            {jobList.map((job, index) => {
+                                return (
+                                    <li key={index}>
+                                        {job.title}
+                                    </li>
+                                );
+                            })}
+                        </ul>
+            </>
         )
     }
-}
+
+
 
 export default JobSearchForm;
--- src/components/SearchCard.jsx
+++ src/components/SearchCard.jsx
@@ -1,18 +1,7 @@
 import { BrowserRouter as Link, useRouteMatch } from "react-router-dom";
 
 const SearchCard = (props) => {
-    const { language } = props;
-    console.log(language);
-
-    const { url } = useRouteMatch
-
-    return (
-        <li>
-            <Link to={`${url}`}>
-
-            </Link>
-        </li>
-    )
+   
 }
 
 export default SearchCard;
